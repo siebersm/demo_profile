236b6ca8b6da8b4187a1101484c34eefc707f04d
Jethro <jethro@itshosted.nl>
2016-09-24 07:52:26 +0200

    Puppetmaster code

--- manifests/demo/puppetmaster.pp
+++ manifests/demo/puppetmaster.pp
@@ -2,6 +2,36 @@ class demo_profile::demo::puppetmaster {
 
   tag 'demo_profile_demo_puppetmaster'
 
-  # Put here your includes to other profiles or puppet code
+  class { 'puppetdb':
+    listen_address  => 'puppetmaster.example.com',
+    manage_firewall => false,
+  }
+  class { 'puppetdb::master::config':
+    manage_report_processor => true,
+    enable_reports          => true,
+  }
+
+  # Configure Apache on this server
+  class { 'apache':
+    purge_configs => true,
+    mpm_module    => 'prefork',
+    default_vhost => true,
+    default_mods  => false,
+  }
+
+  class { 'apache::mod::wsgi': }
+
+  # Configure Puppetboard
+  class { 'puppetboard':
+    puppetdb_host     => $::ipaddress_eth0,
+    manage_git        => true,
+    manage_virtualenv => false,
+  }
+
+  # Access Puppetboard through 
+  class { 'puppetboard::apache::vhost':
+    vhost_name => 'pboard.example.com',
+    port       => 80,
+  }
 
 }
