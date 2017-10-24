02c3931c884deb1ccab103cb0744151c43404e6f
siebersm <marcel@siebers.it>
2017-10-24 23:27:50 +0200

    added some useless text

--- manifests/demo/client.pp
+++ manifests/demo/client.pp
@@ -2,6 +2,6 @@ class demo_profile::demo::client {
 
   tag 'demo_profile_demo_client'
 
-  # Include here all needed profiles or puppet code
+  # Include here all needed profiles or puppet code okedan
 
 }
